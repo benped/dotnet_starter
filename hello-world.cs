2a5fce65f98358caf5a03ef48839fc9d5427ad2c
Ben Pedrick <bpedrick@gmail.com>
2022-03-28 16:06:54 -0500

    declared first string and consoled it

---
+++ hello-world.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// Making a string
+string message1 = "Hello to this new string!";
+
+//Consoling it out
+Console.WriteLine(message1);
