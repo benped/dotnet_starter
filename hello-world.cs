078c4da09bc511da3d5c49d09a71a79757ccda9e
Ben Pedrick <bpedrick@gmail.com>
2022-03-28 16:23:39 -0500

    numbers working '

--- hello-world.cs
+++ hello-world.cs
@@ -6,3 +6,25 @@ string message1 = "Hello to this new string!";
 
 //Consoling it out
 Console.WriteLine(message1);
+
+// // Strings are immutible:
+// string s1 = "Hello ";
+// string s2 = s1;
+// s1 += "World";
+
+// System.Console.WriteLine(s2);
+// //Output: Hello
+
+byte b1 = 255;
+// byte b2 = 256;
+
+short s1 = 32000;
+
+// Console.WriteLine(Byte.MaxValue);//255
+// Console.WriteLine(Byte.MinValue);//0
+// Console.WriteLine(SByte.MaxValue);//127
+// Console.WriteLine(SByte.MinValue);//-128
+Console.WriteLine(b1 + b1); // This adds b1 and writes to console 
+Console.WriteLine(b1 + s1); // you can add different types of numbers
+
+
